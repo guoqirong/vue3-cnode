370454f86ad25a6892377d9316eaea5e244a2bdd
guoqirong <2834964850@qq.com>
2022-04-29 10:54:57 +0800

    开启gzip打包

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,20 @@
 const AutoImport = require('unplugin-auto-import/webpack');
 const Components = require('unplugin-vue-components/webpack');
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+const productionGzipExtensions = ['js', 'css'];
+const gzipPlugins = process.env.NODE_ENV === 'production' ? [
+  // 使用Gzip压缩文件
+  // 报错："TypeError: Cannot read property 'tapPromise' of undefined"是compression-webpack-plugin版本问题，建议使用5.0.1
+  new CompressionPlugin({
+    filename: '[path].gz[query]',
+    algorithm: 'gzip',
+    test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+    threshold: 10240,
+    minRatio: 0.4
+  })
+] : [];
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
@@ -14,7 +28,8 @@ module.exports = {
       }),
       Components({
         resolvers: [ElementPlusResolver()]
-      })
+      }),
+      ...gzipPlugins
     ]
   }
 }
